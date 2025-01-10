ee3a1df67ba2fb3905b2316667c10dff656bc9b4
JunglTemple <tubikardu@gmail.com>
2025-01-10 18:52:04 +0000

    Second commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), react()],
+
+  adapter: vercel()
+});
